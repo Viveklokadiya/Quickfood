73bf807a62531ab4492d5ef4a0b419bdd329be1f
Viveklokadiya <vivekl4517@gmail.com>
2024-03-18 22:32:27 +0530

    Update Navbar.jsx

--- foodi-client/src/components/Navbar.jsx
+++ foodi-client/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ import useAuth from "../hooks/useAuth";
 
 const Navbar = () => {
   const [isSticky, setSticky] = useState(false);
-  const {user, loading} = useAuth();
+  const { user, loading } = useAuth();
   const [cart, refetch] = useCart();
 
   useEffect(() => {
@@ -52,7 +52,7 @@ const Navbar = () => {
           </ul>
         </details>
       </li>
-      
+
       <li>
         <a href="/order">Orders</a>
       </li>
@@ -63,11 +63,10 @@ const Navbar = () => {
       className={`max-w-screen-2xl container mx-auto fixed top-0 left-0 right-0 transition-all duration-300 ease-in-out`}
     >
       <div
-        className={`navbar xl:px-24 ${
-          isSticky
+        className={`navbar xl:px-24 ${isSticky
             ? "shadow-md bg-base-100 transition-all duration-300 ease-in-out"
             : ""
-        }`}
+          }`}
       >
         <div className="navbar-start">
           <div className="dropdown justify-between">
@@ -118,43 +117,43 @@ const Navbar = () => {
               />
             </svg>
           </button>
-         
-         {/* shopping cart */}
-         <Link to="/cart-page">
-         <label
-            tabIndex={0}
-            className="btn btn-ghost btn-circle  lg:flex items-center justify-center mr-3"
-          >
-            <div className="indicator">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                />
-              </svg>
-              <span className="badge badge-sm indicator-item">{cart.length || 0}</span>
-            </div>
-          </label>
-         </Link>
+
+          {/* shopping cart */}
+          <Link to="/cart-page">
+            <label
+              tabIndex={0}
+              className="btn btn-ghost btn-circle  lg:flex items-center justify-center mr-3"
+            >
+              <div className="indicator">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                  />
+                </svg>
+                <span className="badge badge-sm indicator-item">{cart.length || 0}</span>
+              </div>
+            </label>
+          </Link>
 
           {/* login button */}
 
-          { 
+          {
             user ? <>
-           <Profile user={user}/>
-          </> : <button onClick={()=>document.getElementById('my_modal_5').showModal()} className="btn flex items-center gap-2 rounded-full px-6 bg-green text-white">
-            <FaRegUser /> Login
-          </button>
+              <Profile user={user} />
+            </> : <button onClick={() => document.getElementById('my_modal_5').showModal()} className="btn flex items-center gap-2 rounded-full px-6 bg-green text-white">
+              <FaRegUser /> Login
+            </button>
           }
-          <Modal/>
+          <Modal />
         </div>
       </div>
     </header>
